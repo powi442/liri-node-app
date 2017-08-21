244ff0a147e8c18670d3484a75ae2834bf209f64
powi442 <k08ip01@gmail.com>
2017-08-21 09:37:28 -0700

    added twitter key

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'TD6s5VXi6LZDIQy827pr7mtqm',
+  consumer_secret: 'rgNm7ZDqgYXlIu8X9v2yxpbTn9upMnIgb3AHTkLqoJcR1V1mPV',
+  access_token_key: '899380807097176064-8AVVDk3mvX0jdpCwh4rfUOxJ5xOafkS',
+  access_token_secret: '6B0VoPOaBHX4lJF0CCDf5aItbWuy6fv4DhGmGrA21QqLP',
+}
