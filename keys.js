29c046cb560a4a5d245f912ab893335aff944574
powi442 <k08ip01@gmail.com>
2017-08-21 21:39:34 -0700

    twitter working, spotify not so much

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: 'TD6s5VXi6LZDIQy827pr7mtqm',
   consumer_secret: 'rgNm7ZDqgYXlIu8X9v2yxpbTn9upMnIgb3AHTkLqoJcR1V1mPV',
   access_token_key: '899380807097176064-8AVVDk3mvX0jdpCwh4rfUOxJ5xOafkS',
   access_token_secret: '6B0VoPOaBHX4lJF0CCDf5aItbWuy6fv4DhGmGrA21QqLP',
+}
+
+exports.spotifyKeys = {
+  id: '5160035b31d14e758db04580b63285db',
+  secret: '956a977334bc4a0e961fe0ab29534be6',
 }
--- liri.js
+++ liri.js
@@ -1,1 +1,66 @@
-.
+var Twitter= require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var inquirer = require('inquirer');
+
+var keys= require("./keys.js")
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+var client2 = new spotify({
+  id: keys.spotifyKeys.id,
+  secret: keys.spotifyKeys.secret
+});
+
+inquirer.prompt([
+  {
+    type: "list",
+    name: "doWhat",
+    message: "What would you like to do?",
+    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+  }
+]).then(function(user) {
+
+  if (user.doWhat === "my-tweets") {
+
+    var params = {
+      q: 'pow_red_rabbit',
+      count: 20
+    };
+
+    client.get('search/tweets', params, gotData);
+    function gotData(err, data, response) {
+      var tweets = data.statuses;
+      for (var i = 0; i < tweets.length; i++){
+        console.log("Ian's tweets: "+ tweets[i].text + "\nCreated on: " + tweets[i].created_at);
+        }
+      };
+    }
+  if (user.doWhat === "spotify-this-song"){
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "songName",
+        message: "What song would you like me to check out?"
+      }
+    ]).then(function(user) {
+
+      client2.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+      console.log(data);
+      });
+    })
+
+
+    }
+
+});
